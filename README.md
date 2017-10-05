8ab7dacedbe5a61fa17a2716818f86d73c0856a9
jgwilson42 <james.wilson@diffblue.com>
2017-10-05 21:15:36 +0100

    Initial Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ecommerce-demo
+# E-commerce Demo
+
+Demo of a Java e-commerce site using Spring. 
+
+To build type:
+  mvn compile
+
+To test type:
+  mvn test
+
+To run type:
+  mvn spring-boot:run
+
+Copyright 2016-2017 DiffBlue limited. All rights reserved.
---
+++ src/main/java/com/diffblue/demo/ecommerce/Application.java
@@ -0,0 +1,13 @@
+package com.diffblue.demo.ecommerce;
+
+// Copyright 2016-2017 DiffBlue limited. All rights reserved.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}

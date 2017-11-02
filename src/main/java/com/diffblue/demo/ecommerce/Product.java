721a2d6d2b8272cfd78c44f99aeba4c5e242a126
sara-db <sara.cruz@diffblue.com>
2017-11-02 19:48:23 +0000

    Introduction to product categories

--- src/main/java/com/diffblue/demo/ecommerce/Product.java
+++ src/main/java/com/diffblue/demo/ecommerce/Product.java
@@ -10,6 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -46,6 +48,13 @@ public class Product {
   @Column(name = "description")
   @NotEmpty
   private String description;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+
+
 
   public String getName() {
     return this.name;
@@ -118,4 +127,20 @@ public class Product {
   public void setSku(String sku) {
     this.sku = sku;
   }
+
+  public Category getCategory() {
+    return this.category;
+  }
+
+  /**
+   * Update the product category.
+   * @param category new category.
+   * @return future proof and consistent with other setters
+   */
+
+  public boolean setCategory(Category category) {
+    this.category = category;
+    return true;
+  }
+
 }
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,12 @@
 -- Copyright 2016-2017 DiffBlue limited. All rights reserved.
 -- Sample data for testing / demo purposes
-INSERT INTO products(sku, name, price, description) VALUES ('PROD001', 'foo', '9.99','description1');
-INSERT INTO products(sku, name, price, description) VALUES ('PROD002', 'bar','5.99','description2');
+
+-- Categories
+INSERT INTO categories(name) VALUES ('Apparel');
+INSERT INTO categories(name) VALUES ('Pins');
+INSERT INTO categories(name) VALUES ('Stickers');
+INSERT INTO categories(name) VALUES ('Prints');
+
+-- Products
+INSERT INTO products(sku, category_id, name, price, description) VALUES ('PROD001', (SELECT id from categories where name='Apparel'),'foo', '9.99','description1');
+INSERT INTO products(sku, category_id, name, price, description) VALUES ('PROD002', (SELECT id from categories where name='Pins'),'bar','5.99','description2');

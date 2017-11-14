219597951a446de61f6f5360f927f76e9c74e060
sara-db <sara.cruz@diffblue.com>
2017-11-14 16:16:13 +0000

    Remove unused import

--- src/main/java/com/diffblue/demo/ecommerce/Customer.java
+++ src/main/java/com/diffblue/demo/ecommerce/Customer.java
@@ -4,7 +4,6 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 public class Customer {
 

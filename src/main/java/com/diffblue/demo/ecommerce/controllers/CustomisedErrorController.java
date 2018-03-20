2b990d0e15c9cec3a84ddc3dad648fd6eb972054
Justin Yu <justin.yu@diffblue.com>
2018-03-20 16:26:50 +0000

    [TG-2836] Initial tidy-up of custom error page

--- src/main/java/com/diffblue/demo/ecommerce/controllers/CustomisedErrorController.java
+++ src/main/java/com/diffblue/demo/ecommerce/controllers/CustomisedErrorController.java
@@ -1,9 +1,5 @@
 package com.diffblue.demo.ecommerce.controllers;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
@@ -12,34 +8,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class CustomisedErrorController implements ErrorController {
 
-    private final ErrorAttributes attributes;
+  private final ErrorAttributes attributes;
 
-    public CustomisedErrorController(ErrorAttributes attributes) {
-        this.attributes = attributes;
-    }
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
+  public CustomisedErrorController(ErrorAttributes attributes) {
+    this.attributes = attributes;
+  }
 
-    @RequestMapping("/error")
-    public String customError(Model model, HttpServletRequest request) {
-        Map<String, Object> errormap = retriveErrorAttributes(true, request);
-        model.addAttribute("status",errormap.get("status"));
-        model.addAttribute("error",errormap.get("error"));
-        model.addAttribute("message",errormap.get("message"));
-        model.addAttribute("timestamp",errormap.get("timestamp"));
-        model.addAttribute("path",errormap.get("path"));
-        model.addAttribute("title","Some thing went wrong");
-        model.addAttribute("solution","try after some time. If the issue persists then create a trouble ticket.");
-        return"CustomisedError";
-    }
-    private Map<String, Object> retriveErrorAttributes(boolean includeStackTrace, HttpServletRequest httpServletRequest){
-        RequestAttributes requestAttributes = new ServletRequestAttributes(httpServletRequest);
-        Map<String, Object> errormap = this.attributes.getErrorAttributes(requestAttributes, includeStackTrace);
-        return errormap;
-    }
+  /**
+   * Calls the /error endpoint when an error arises.
+   */
+  @Override
+  public String getErrorPath() {
+    return "/error";
+  }
+
+  /**
+   * Displays the CustomisedError.html page and gets the details of the Http error.
+   */
+  @RequestMapping("/error")
+  public String customError(Model model, HttpServletRequest request) {
+    Map<String, Object> errormap = retriveErrorAttributes(true, request);
+    model.addAttribute("status",errormap.get("status"));
+    model.addAttribute("error",errormap.get("error"));
+    model.addAttribute("message",errormap.get("message"));
+    model.addAttribute("timestamp",errormap.get("timestamp"));
+    model.addAttribute("path",errormap.get("path"));
+    return "CustomisedError";
+  }
+
+  private Map<String, Object> retriveErrorAttributes(boolean includeStackTrace,
+                                                     HttpServletRequest httpServletRequest) {
+    RequestAttributes requestAttributes = new ServletRequestAttributes(httpServletRequest);
+    Map<String, Object> errormap = this.attributes.getErrorAttributes(requestAttributes,
+            includeStackTrace);
+    return errormap;
+  }
 }
